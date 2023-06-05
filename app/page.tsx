7eacf012a7029ca7febc7340cc915d17533c6d84
saneyoshi <saneyoshi@collabos.com>
2023-06-05 12:23:31 +0900

    add:API fetch & Loading create.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import NotesList from "./_components/notes-list";
+
 export default function Home() {
   return (
     <main>
       <div className="m-10 text-center">
-        Hello World!
+        <p>Hello World!</p>
+        <NotesList />
       </div>
     </main>
   )
