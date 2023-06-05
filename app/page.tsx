90074b249a343247e424a1f0b6aa46b20b310916
saneyoshi <saneyoshi@collabos.com>
2023-06-05 12:51:58 +0900

    add:Refresh(server component, client component) create.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import NotesList from "./_components/notes-list";
 import TimerCounter from "./_components/timer-counter";
-
+import { Suspense } from "react";
+import Spinner from './_components/spinner';
+import RefreshBtn from "./_components/refresh-btn";
 
 export default function Home() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Hello World!</p>
-        <NotesList />
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          <NotesList />
+        </Suspense>
         <TimerCounter />
+        <RefreshBtn />
       </div>
     </main>
   )
